348ec8fb56919247135bc0c5aeb91d89995c60c9
mateovelmontes <oleg.eliiashiv@cloudcod.com>
2024-07-08 10:14:27 +0300

    Have to write README

--- consumer_aio_pika.py
+++ consumer_aio_pika.py
@@ -1,15 +1,13 @@
 import asyncio
 import aio_pika
-import uuid
 import json
-from datetime import datetime
-import os
 import logging
+import os
 RABBIT_MQ_HOST_ADDRESS = os.getenv('RABBIT_MQ_HOST_ADDRESS', '127.0.0.1')
 RABBIT_MQ_PORT = int(os.getenv('RABBIT_MQ_PORT', 5672))
 RABBIT_MQ_USERNAME = os.getenv('RABBIT_MQ_USERNAME', 'guest')
 RABBIT_MQ_PASSWORD = os.getenv('RABBIT_MQ_PASSWORD', 'guest')
-PUBLISH_DELAY = float(os.getenv('PUBLISH_DELAY', 1))
+
 logging.basicConfig(
     format='%(asctime)s : %(levelname)s: %(funcName)-15s : %(message)s',
     level=logging.INFO,
@@ -20,16 +18,17 @@ logging.basicConfig(
 )
 
 
-async def publish_message(channel):
-    message = {
-        "message_id": str(uuid.uuid4()),
-        "created_on": datetime.now().isoformat()
-    }
-    await channel.default_exchange.publish(
-        aio_pika.Message(body=json.dumps(message).encode()),
-        routing_key="test_queue"
-    )
-    logging.info(f"[>] Sent: {message}")
+async def consume_message(message: aio_pika.IncomingMessage):
+    try:
+        body = json.loads(message.body.decode())
+        logging.info(f"[<] Received message: {body}")
+        # Message approve
+        await message.ack()
+        logging.info("[+] Message acknowledged")
+    except Exception as e:
+        # Message rejected
+        await message.reject(requeue=True)
+        logging.info(f"[!] Error processing message: {e}. Message returned to the queue")
 
 
 async def main():
@@ -41,13 +40,13 @@ async def main():
         reconnect_attempts=10,
         reconnect_interval=30,
     )
-    channel = await connection.channel()
-    # Create the  queue (if not exist)
-    queue = await channel.declare_queue("test_queue", durable=True)
-    logging.info(f"[x] Producer started to publish messages every {PUBLISH_DELAY} seconds")
-    while True:
-        await publish_message(channel)
-        await asyncio.sleep(PUBLISH_DELAY)
+    async with connection:
+        channel = await connection.channel()
+        # Create the  queue (if not exist)
+        queue = await channel.declare_queue("test_queue", durable=True)
+        await queue.consume(consume_message)
+        logging.info("[x] Consumer awaiting for messages ...")
+        await asyncio.Future()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
